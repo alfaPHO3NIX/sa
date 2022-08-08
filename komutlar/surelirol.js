386755ed6cbb66e8ddd5f48c82611cb4f803c4ad
Glitch (futuristic-chemical-quality) <none>
2022-08-08 20:57:33 +0000

    🚄🚇 Checkpoint ./komutlar/surelirol.js:31098313/15

--- komutlar/surelirol.js
+++ komutlar/surelirol.js
@@ -12,11 +12,11 @@ Rolü Veren Yetkili;
 <@authorID>
 
 Süre;
-$noMentionMessage[3]] Saniye
+$message[3] Saniye]
 $footer[Made By xFurkan_]
 
-$argsCheck[<3;Bir Süre Belirtmelisin!]
+$argsCheck[3;Bir Süre Belirtmelisin!]
 $argsCheck[>2;Verilecek rolü etiketlemelisin!]
 $argsCheck[>1;Rol verilecek kişiyi etiketlemelisin!]
 $onlyPerms[admin;Yetkin Yok.]
-`}
+`}ğ
