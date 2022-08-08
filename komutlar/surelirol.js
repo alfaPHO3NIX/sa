4394705e19f93c35cb5c80420b521b21ffa3a35b
Glitch (futuristic-chemical-quality) <none>
2022-08-08 22:17:48 +0000

    👲🍽 Checkpoint ./komutlar/surelirol.js:31098313/785 ./package.json:31098313/2

---
+++ komutlar/surelirol.js
@@ -0,0 +1,26 @@
+module.exports = {
+name:"sürelirol",
+code:`
+$takeRole[$guildID;$mentioned[1];$mentionedRoles[1]]
+$wait[$message[3]]
+$channelSendMessage[$channelID;;{newEmbed:
+{title:Başarılı!}
+{description:
+Verilen Rol
+<@&$mentionedRoles[1]>
+
+Rolü Veren Yetkili
+<@$authorID>
+
+Rolü Verilen Kişi
+<@$mentioned[1]>
+
+Süre
+$message[3] $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$message[3];y; Yıl];d; Gün];h; Saat];m; Dakika];s; Saniye]
+}}]
+$giveRole[$guildID;$mentioned[1];$mentionedRoles[1]]
+$onlyIf[$checkContains[$noMentionMessage[1];s;m;h;d;w]!=false;**<@$authorID> Lütfen Bir Zaman Dilimi Belirt**]
+$onlyIf[$findRole[$mentionedRoles[1]]!=;<@$authorID> Lütfen Bir Rol Etiketle]
+$onlyIf[$mentioned[1;no]!=undefined;**<@$authorID> Lütfen Bir Kişi Etiketle**]
+`
+}
