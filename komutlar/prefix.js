be1fe579a4e74c65531f711da56d1b90aaacd344
Glitch (futuristic-chemical-quality) <none>
2022-08-08 13:03:11 +0000

    🚩💎 Checkpoint ./tokyo.js:31098313/22 ./komutlar/prefix.js:31098313/186

--- komutlar/prefix.js
+++ komutlar/prefix.js
@@ -1,6 +1,7 @@
 module.exports = {
 name: "prefix",
 code: `
+$argsCheck[<1;Yeni prefixin ne olacağını yazmalısın!]
 $onlyForIDs[$botOwnerID;Selam]
 $setServerVar[prefix;$message]
 Yeni Prefix: $message
