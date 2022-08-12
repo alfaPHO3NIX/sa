5fcbe9d9448bfd5202bd69fa21194319f5e76e9b
Glitch (futuristic-chemical-quality) <none>
2022-08-12 14:59:20 +0000

    🍗🏢 Checkpoint ./komutlar/paraver.js:31098313/5 ./komutlar/param.js:31098313/64

--- komutlar/param.js
+++ komutlar/param.js
@@ -5,6 +5,7 @@ $title[$getVar[botadi]]
 $description[** **
 Mevcut $getVar[parasembol]: $getGlobalUserVar[para]
 ** **]
+$footer[Bakiyesi Gösterilen Kişi: <@$mentioned[1;yes]>]
 $color[RANDOM]
 `
 }
