ceab72b75c514d04bf3b12b20efe610b95277e07
Glitch (futuristic-chemical-quality) <none>
2022-08-12 16:56:31 +0000

    🐏💋 Checkpoint ./komutlar/paragonder.js:31098313/89

--- komutlar/paragonder.js
+++ komutlar/paragonder.js
@@ -2,8 +2,8 @@ module.exports = {
 name: "paragönder",
 aliases: ['para gönder'],
 code: `
-$setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$authorID];$noMentionMessage[1]]]
-$setGlobalUserVar[para;$sum[$getGlobalUserVar[para;$mentioned[1;no]];$noMentionMessage[1]]];$mentioned[1;no]]
+$setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$authorID];$noMentionMessage[1]];$authorID]
+$setGlobalUserVar[para;$sum[$getGlobalUserVar[para;$mentioned[1;no]];$noMentionMessage[1]];$mentioned[1;no]]
 
 $onlyIf[$isNumber[$noMentionMessage[1]]!=false;Bir sayı belirtmelisin!]
 $onlyIf[$mentioned[1;no]!=undefined;Bir kişi belirtmelisin!]
