71af2385c5807f5d2a1195044a80a8274fd169ec
Glitch (futuristic-chemical-quality) <none>
2022-08-14 20:45:46 +0000

    🏔🎢 Checkpoint ./komutlar/presurem.js:31098313/14

--- komutlar/presurem.js
+++ komutlar/presurem.js
@@ -3,6 +3,7 @@ name: "presürem",
 code: `
 $deletecommand
 $title[Premium]
-$description[\nKalan Premium Süresi;\n$getGlobalUserVar[presure]]
+$description[\nKalan Premium Süresi;
+$getVar[presurem]]
 `
 }
