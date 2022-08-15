26cb2540772965ee0ddf6eb59d0b69043485bf63
Glitch (futuristic-chemical-quality) <none>
2022-08-15 20:07:16 +0000

    🌻🙇 Checkpoint ./shopier/siparisreddet.js:31098313/203 ./server.js:31098313/408 ./shopier/siparisonayla.js:31098313/340

---
+++ shopier/siparisonayla.js
@@ -0,0 +1,9 @@
+module.exports = [{
+  type: "interaction",
+    prototype: "slash",
+      name: "onayla",
+        code: `
+        
+                        $interactionReply[✅ Sipariş Reddedildi]
+`
+}]
---
+++ shopier/siparisreddet.js
@@ -0,0 +1,8 @@
+module.exports = [{
+  type: "interaction",
+    prototype: "button",
+      name: "reddet",
+        code: `
+        Reddedildi
+`
+}]
