8427afa4874456d472172b295f6142d364efde9d
Glitch (futuristic-chemical-quality) <none>
2022-08-15 17:23:16 +0000

    👞🐙 Checkpoint ./shopier/satinal.js:31098313/120

--- shopier/satinal.js
+++ shopier/satinal.js
@@ -5,8 +5,10 @@ $deletecommand
 $description[
 **$message[1] Sayılı Sipariş No** Başarıyla Sıraya Alınmıştır
 
+Kontrol No:
+
 Not: **Sipariş No yanlış çıkarsa size bot tarafından bildirim gelecektir. eğer gelmezse dm'niz kapalıdır.**]
 $onlyIf[$isNumber[$message[1]]!=false;**Shopier Sipariş No** Girmelisin!]
-
+$createVar[main;$random[10000;99999]-siparisno;$message[1]]
 `
 }
