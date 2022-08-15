8628ac1c6cdc6b8e35fe28c578a7cda9891d04d2
Glitch (futuristic-chemical-quality) <none>
2022-08-15 22:16:21 +0000

    🌓🏎 Checkpoint ./shopier/sipariskontrol.js:31098313/353

--- shopier/sipariskontrol.js
+++ shopier/sipariskontrol.js
@@ -2,5 +2,9 @@ module.exports = {
 name: "siparişkontrol",
 code: `
 $onlyIf[$isNumber[$message[1]]!=false;**Kontrol No** girmelisin!]
+Kontrol No: $getVar[$mesaage[1]kontrolno]
+Siparişi Oluşturan: <@$getVar[$message[1]sahip]>
+Durumu: $getVar[$message[1]durum]
+$supressErrors[Bir Sorun Oluştu! komutu kontrol et kontrol No'yu yazmamış olabilirsin.]
 `
 }
