bd6d059b7a8172ef1eb66b8b911878d95745c040
Glitch (futuristic-chemical-quality) <none>
2022-08-15 12:50:56 +0000

    🌧🚗 Checkpoint ./shopier/satinal.js:31098313/597

--- shopier/satinal.js
+++ shopier/satinal.js
@@ -1,6 +1,13 @@
 module.exports = {
 name: "satınal",
 code: `
-$onlyIf[$isNumber[[$message[1]]]!=false;**Shopier Sipariş No** girmelisin!]]
+$deletecommand
+$description[
+**$message[1] Sayılı Sipariş No** Başarıyla Sıraya Alınmıştır
+
+Not: **Sipariş No yanlış çıkarsa size bıt tarafından bildirim gelecektir. eğer gelmezse dm'niz kapalıdır.**]
+$onlyIf[$isNumber[$message[1]]!=false;**Shopier Sipariş No** Girmelisin!]
+$onlyIf[$channelType==dm;Lütfen bu komutu botun dmsinde kullanın.]
+$deleteIn[5s]
 `
 }
