7a99645a5e10d5e49dd8a4be8344cc99b8591f2f
Glitch (futuristic-chemical-quality) <none>
2022-08-15 19:31:16 +0000

    👢🚇 Checkpoint ./shopier/satinal.js:31098313/732

--- shopier/satinal.js
+++ shopier/satinal.js
@@ -3,26 +3,26 @@ module.exports = [{
     prototype: "slash",
       name: "satinal",
         code: `
-        $interactionReply[✅ <@$authorID>, başvurun iletildi!]
+                $interactionReply[✅ <@$authorID>, başvurun iletildi!]
         $interactionModal[Yetkili alım;yetkiliModal;{actionRow:
-        {textInput:Ad Soyad:1:isimModal:yes:Ad Soyad:3:30}}
+        {textInput:İsminiz ve yaşınız?:1:isimModal:yes:Şafak 14:3:20}}
         {actionRow:
-         {textInput:Sipariş No Giriniz:1:siparisnoModal:yes:123456789:9:9}}
-    ]
+        {textInput:Kaç saat aktif olabilirsiniz?:1:siparisnoModal:yes:7/24:1:10}}
+     ]
         `
         },{
           type: "interaction",
             prototype: "modal",
               name: "yetkiliModal",
                 code: `
-               $dm[$botOwnerID]
                 $color[1;RANDOM]
                 $author[1;$userTag[$authorID];$authorAvatar]
                 $thumbnail[1;$serverIcon]
-                $description[1;<@$authorID> **<@$authorID>** adlı kişi Sipariş Teslim Formu Oluşturdu]
+                $description[1;**<@$authorID>** adlı kişi Sipariş Teslim Formu Oluşturdu]
                $addTimestamp[1]
-$addField[Sipariş NO;$textInputValue[whyModal]]
-$addField[Ad Soyad;$textInputValue[typeModal]]
+              $useChannel[1008817326239527052]
+$addField[Sipariş NO;$textInputValue[siparisnoModal]]
+$addField[Ad Soyad;$textInputValue[isimModal]]
 
 `
   }]
