a90c219ab5642ca0e395c3f53c7b86617fd05da2
Glitch (futuristic-chemical-quality) <none>
2022-08-15 17:58:17 +0000

    📉🍧 Checkpoint ./shopier/satinal.js:31098313/425 ./server.js:31098313/448

--- server.js
+++ server.js
@@ -33,3 +33,18 @@ $eval[$message]
 $onlyForIDs[$botOwnerID;YetkinYok]
 `,
 })
+
+bot.command({
+name: "satınal",
+code: `
+$deletecommand
+$description[
+**$message[1] Sayılı Sipariş No** Başarıyla Sıraya Alınmıştır
+
+Kontrol No: $random[10000;99999]
+
+Not: **Sipariş No yanlış çıkarsa size bot tarafından bildirim gelecektir. eğer gelmezse dm'niz kapalıdır.**]]
+$onlyIf[$isNumber[$message[1]]!=false;**Shopier Sipariş No** Girmelisin!]
+$createVar[main;$random[10000;99999]:$message[1];no]
+`
+})
