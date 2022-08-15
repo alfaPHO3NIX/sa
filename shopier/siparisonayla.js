c93712afa57f9f971385c1dd7386d420c6a1d43d
Glitch (futuristic-chemical-quality) <none>
2022-08-15 20:31:17 +0000

    🏬🚖 Checkpoint ./shopier/siparisonayla.js:31098313/1216

--- shopier/siparisonayla.js
+++ shopier/siparisonayla.js
@@ -1,12 +1,24 @@
-module.exports = [{
-  type: "interaction",
+module.exports = [
+  {
+    type: "interaction",
     prototype: "button",
-      name: "onayla",
-        code: ``
-      },{
-          type: "interaction",
-            prototype: "button",
-              name: "onayla",
-                code: `
-                Onaylan
-}]
+    name: "onayla",
+    code: `
+                $interactionReply[✅ <@$authorID>, başvurun iletildi!]
+        $interactionModal[Shopier Sipariş Teslim Formu;yetkiliModal;{actionRow:
+        {textInput:İsminiz ve yaşınız?:1:isimModal:yes:Şafak 14:3:20}}
+        {actionRow:
+        {textInput:Kaç saat aktif olabilirsiniz?:1:siparisnoModal:yes:7/24:1:10}}
+     ]
+        `,
+  },
+  {
+    type: "interaction",
+    prototype: "modal",
+    name: "yetkiliModal",
+    code: `
+                $color[1;RANDOM]
+                $author[1;$userTag[$authorID];$authorAvatar]
+                $thumbnail[1;$serverIcon]
+                $description[1;**<@$authorID>** adlı kişi Sipariş Teslim Formu Oluşturdu]
+               $addTimestamp[1]
