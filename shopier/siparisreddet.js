704ee144c8b193f5194603ebec11aac8ecbc605f
Glitch (futuristic-chemical-quality) <none>
2022-08-16 00:39:28 +0000

    💃👐 Checkpoint ./shopier/siparisreddet.js:31098313/2079

--- shopier/siparisreddet.js
+++ shopier/siparisreddet.js
@@ -5,23 +5,23 @@ module.exports = [
     name: "hayır",
     code: `
                 $interactionReply[✅ <@$authorID>, başvurun iletildi!]
-        $interactionModal[Shopier Sipariş Onay;kontrolreddetModal;{actionRow:
+        $interactionModal[Shopier Sipariş Onay;kontrolnoreddetModal;{actionRow:
         {textInput:Kontrol No:1:reddetkontrolnoModal:yes:123456789:9:50}}
          {actionRow:
-          {textInput:Kullanıcı id:1:reddetuseridModal:yes:Kullanıcı İd:9:50}}
+          {textInput:Kontrol No:1:reddetuseridModal:yes:Kullanıcı İd:9:50}}
            {actionRow:
-            {textInput:Reddedilme Sebebi:1:siparisreddetsebepModal:yes:Reddedilme Sebebi:9:50}}
+            {textInput:Kontrol No:1:siparisreddetsebepModal:yes:Reddedilme Sebebi:9:50}}
      ]
         `,
   },
   {
     type: "interaction",
     prototype: "modal",
-    name: "kontrolnoredddetModal",
+    name: "kontrolnoreddetModal",
     code: `
     $dm[$textInputValue[reddetuseridModal]]
-    Selam <@$textInputValue[reddetuseridModal]>
-    **$textInputValue[reddetkontrolnoModal]** Kontrol No'lu Siparişin Onaylandı!
+    $title[Selam <@$textInputValue[reddetuseridModal]>]
+    **$textInputValue[reddetkontrolnoModal]** Kontrol No'lu Siparişin Reddedildi :(
     
     Reddedilme sebebi: $textInputValue[siparisreddetsebepModal]
     `
