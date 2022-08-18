af8290533ae71ef9648e4a6d5372bae041a4fefb
Glitch (futuristic-chemical-quality) <none>
2022-08-18 22:49:56 +0000

    🛬🏋 Checkpoint ./komutlar/sıralama.js:31098313/265

---
+++ komutlar/sıralama.js
@@ -0,0 +1,8 @@
+module.exports = {
+name: "sıralama para",
+aliases: ['siralama para','leaderboard money'],
+code: `
+$title[$customEmoji[coin] Para Sıralaması]
+$description[$globalUserLeaderBoard[para]]
+`
+}
