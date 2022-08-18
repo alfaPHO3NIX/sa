f912647478e9cd708b1dec628d794af11f7feb29
Glitch (futuristic-chemical-quality) <none>
2022-08-18 09:03:13 +0000

    🌸🔑 Checkpoint ./kasa/katlamakasası.js:31098313/151

--- kasa/katlamakasası.js
+++ kasa/katlamakasası.js
@@ -10,10 +10,11 @@ $title[Kaybettin :(]
 $description[Üzgünüm,maalesef kaybbetin
 Bahis Koyduğun Miktar: $getUserVar[katlamakasasicarpan]]
 ;
-$title[Kaybettin :(]
-$description[Üzgünüm,maalesef kaybbetin
+$title[Tebrikler! Kazandın]
+$description[Çok Şanslısın! Kazandın]
 Bahis Koyduğun Miktar: $getUserVar[katlamakasasicarpan]]
-]
+$eval[SelamKe]
+
 $setUserVar[katlamakasasicarpan;$noMentionMessage[1]]
 $onlyIf[$isNumber[$message[1]]!=false;Bir Sayı Belirt]
 `
