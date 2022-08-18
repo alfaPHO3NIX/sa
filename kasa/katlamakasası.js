caa2516766995f53e2fbce2948add52eb722248c
Glitch (futuristic-chemical-quality) <none>
2022-08-18 09:25:20 +0000

    🏀🌖 Checkpoint ./kasa/katlamakasası.js:31098313/1919

--- kasa/katlamakasası.js
+++ kasa/katlamakasası.js
@@ -1,19 +1,19 @@
 module.exports = {
-name: "katlamakasası",
-code: `
-$randomText[
-$title[Kaybettin :(]
-$description[Üzgünüm,maalesef kaybbetin
-Bahis Koyduğun Miktar: $getUserVar[katlamakasasicarpan]]
-;
-$title[Kaybettin :(]
-$description[Üzgünüm,maalesef kaybbetin
-Bahis Koyduğun Miktar: $getUserVar[katlamakasasicarpan]]
-;
-$title[Tebrikler! Kazandın]
+  name: "katlamakasası",
+  code: `
+  
+  $title[Tebrikler! Kazandın]
 $description[Çok Şanslısın! Kazandın
 Bahis Koyduğun Miktar: $getUserVar[katlamakasasicarpan]]
 ]
+  
+$onlyIf[$random[1;4]==4;
+$title[Kaybettin :(]
+$description[Üzgünüm,maalesef kaybbetin
+Bahis Koyduğun Miktar: $getUserVar[katlamakasasicarpan]]
+]
+
+
 $setUserVar[katlamakasasicarpan;$noMentionMessage[1]]
 $onlyIf[$isNumber[$message[1]]!=false;Bir Sayı Belirt]
 `
