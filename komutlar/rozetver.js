42508167fc398dadfd2b5b7770868023332f1421
Glitch (futuristic-chemical-quality) <none>
2022-08-18 20:40:56 +0000

    📰🌟 Checkpoint ./komutlar/rozetver.js:31098313/992

--- komutlar/rozetver.js
+++ komutlar/rozetver.js
@@ -1,8 +1,14 @@
 module.exports = {
-name: "rozetver",
-code: `
-
+  name: "rozetver",
+  code: `
+$deletecommand
+$title[Başarılı!]
+$description[<@$mentioned[1]> Adlı kişiye $noMentionMessage[1] Rozeti Verildi!]
+$setGlobalUserVar[$no]
+$deleteIn[5s]
+$argsCheck[>1;Bir Rozet Belirt]
 $onlyIf[$mentioned[1]!=undefined;Lütfen Bir Kişi Belirt]
 $onlyForIDs[$botOwnerID;Yetkin Yok $customEmoji[reddet]]
-`
-}
+$suppressErrors[Bi hata oluştu! Rozet adını girmedin yada girdiğin rozet bulanamadı.]
+`,
+};
