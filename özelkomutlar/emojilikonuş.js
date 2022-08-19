36870b2da27669746cf6af5b7c948d052e0fc8c2
Glitch (futuristic-chemical-quality) <none>
2022-08-19 20:17:13 +0000

    ⛱🏜 Checkpoint ./özelkomutlar/emojilikonuş.js:31098313/95

--- özelkomutlar/emojilikonuş.js
+++ özelkomutlar/emojilikonuş.js
@@ -3,14 +3,14 @@ module.exports = {
   aliases: ["emojilikonuş"],
   code: `
 $deletecommand
-$sendWebhook[$replaceText[$createWebhook[$channelID;$username;$userAvatar];,
-;;;]]
+$sendWebhook[$eval[$replaceText[$createWebhook[$channelID;$username;$userAvatar]]]];,
+;;;$message
 $replaceText[$replaceText[$replaceText[$replaceText[$message
 ;:bughunter:;$customEmoji[bughunter]]
 ;:verified:;$customEmoji[verified]]
 ;:kirmiziyildiz:;$customEmoji[kirmiziyildiz]]
 ;:tac:;$customEmoji[emoji_003]]
-
+]]
 $onlyIf[$getGlobalUserVar[premium]==Var;Bu Özellik Sadece Premium Üyelere Özeldir$customEmoji[reddet]]
 `,
 };
