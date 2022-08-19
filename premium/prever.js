bdae21227ddf15e225e95fb62d5503fe4949704c
Glitch (futuristic-chemical-quality) <none>
2022-08-19 02:45:55 +0000

    🏡🎇 Checkpoint ./premium/prever.js:31098313/592

--- premium/prever.js
+++ premium/prever.js
@@ -1,1 +1,13 @@
-module.exports
+module.exports = {
+name: "prever",
+aliases: ['premiumver'],
+code: `
+$title[$customEmoji[verified]Başarılı]
+$description[<@$mentioned[1;no]> Adlı Kişiye Premium Verildi!
+\n$customEmoji[premium]Premium Rozeti Kazanıldı]
+$setGlobalUserVar[premiumsahibirozet;Sahipsin$customEmoji[onayla];$mentioned[1]]
+$setGlobalUserVar[premium;Var;$mentioned[1]]
+$onlyIf[$mentioned[1;no]!=undefined;Bir Kişi Belirmelisin $customEmoji[reddet]]
+
+`
+}
