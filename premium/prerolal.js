389dcb18761d07e71062de8ff93ee911d4143266
Glitch (futuristic-chemical-quality) <none>
2022-08-19 09:25:44 +0000

    👩🌺 Checkpoint ./premium/prerolal.js:31098313/1

--- premium/prerolal.js
+++ premium/prerolal.js
@@ -2,7 +2,7 @@ module.exports = {
 name: "prerolal",
 aliases: ['premiumrolal'],
 code: `
-
+s
 $onlyForServers[688736441290719256;Bu Komut Sadece **TG MC** adlı sunucuda kullanılabilir$customEmoji[reddet]]
 $onlyIf[$hasRoles[688736441290719256;$authorID;]]
 $onlyIf[$getGlobalUserVar[premium]==Var;Bu Özellik Premium Üyelere Özeldir$customEmoji[reddet]]
