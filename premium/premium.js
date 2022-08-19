294013b154827125e782d47b91edacbc669abccd
Glitch (futuristic-chemical-quality) <none>
2022-08-19 03:39:02 +0000

    👆🏇 Checkpoint ./premium/premium.js:31098313/669

--- premium/premium.js
+++ premium/premium.js
@@ -1,3 +1,17 @@
 module.exports = {
 name: "premium",
-aliases: ,}
+aliases: ['pre'],
+code: `
+$title[$customEmoji[premium]Premium Özellikleri$customEmoji[premium]]
+$description[**          Özel Komutlar**
+$getServerVar[prefix]e / emojilikonuş {mesaj} : 
+Nitronuz olmasa bile emojili bir şekilde mesaj gönderebilirsiniz!
+
+**$getServerVar[prefix]emojiler**;
+Emojili konuş sistemindeki emojileri /  listeler
+
+
+
+]
+`
+}
