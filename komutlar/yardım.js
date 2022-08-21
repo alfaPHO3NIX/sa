6b7af8f00a98d7617bcbec96a6f35ecac266f88e
Glitch (futuristic-chemical-quality) <none>
2022-08-21 17:31:02 +0000

    ☂️😬 Checkpoint ./komutlar/yardım.js:31098313/646

---
+++ komutlar/yardım.js
@@ -0,0 +1,14 @@
+module.exports = {
+name: "",
+code: `
+$title[$customEmoji[kitap] Yardım $customEmoji[kitap]]
+$description[
+**     Ekonomi Komutları**$customEmoji[zumrut]
+
+**$getServerVar[prefix]param / $getServerVar[prefix]param {kişi}**
+Bir kişi etiketlemezseniz kendi bakiyenizi,bir kişi etiketlerseniz etkietlediğiniz kişinin bakiyesini gösterir
+
+**$getServerVar[prefix]günlük / $getServerVar[prefix]daily / **
+]
+`
+}
