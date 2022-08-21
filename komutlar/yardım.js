bea1fd56112bd8c310b1661741189ff74fc93b94
Glitch (futuristic-chemical-quality) <none>
2022-08-21 17:43:00 +0000

    💿💃 Checkpoint ./komutlar/yardım.js:31098313/87

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -8,7 +8,10 @@ $description[
 **$getServerVar[prefix]param / $getServerVar[prefix]param {kişi}**
 Bir kişi etiketlemezseniz kendi bakiyenizi,bir kişi etiketlerseniz etkietlediğiniz kişinin bakiyesini gösterir
 
-**$getServerVar[prefix]günlük / $getServerVar[prefix]daily / **
+**$getServerVar[prefix]günlük / $getServerVar[prefix]daily**
+4 saatte bir para alırsınız.
+
+**$getServerVar[prefix]**
 ]
 `
 }
