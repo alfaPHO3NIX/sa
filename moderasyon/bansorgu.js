48d1da8da311dda1b6732dcd6860185eb05924c2
Glitch (xfrknbot) <none>
2022-09-24 01:44:08 +0000

    🚁🍎 Checkpoint ./moderasyon/bansorgu.js:31098313/104

--- moderasyon/bansorgu.js
+++ moderasyon/bansorgu.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "bansorgu",
   aliases: ["yasaksorgu","yasaklanmasorgu"],
   code: `
-  $onlyIf[$isNumber[1]==true;$title[Hata $customEmoji[reddet]]
+  $onlyIf[$isNumber[$noMentionedMessage[1]]!=false;$title[Hata $customEmoji[reddet]]
   $description[
   Lütfen sorgulanacak kişinin id'sini yazın!
   ]]
