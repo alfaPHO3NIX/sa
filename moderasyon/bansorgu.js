3fc175ac344ce64c1382e931f46833717372bafd
Glitch (xfrknbot) <none>
2022-09-25 11:23:57 +0000

    🐝📫 Checkpoint ./moderasyon/bansorgu.js:31098313/659

--- moderasyon/bansorgu.js
+++ moderasyon/bansorgu.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "bansorgu",
-  aliases: ["yasaksorgu","yasaklanmasorgu"],
+  aliases: ["yasaksorgu", "yasaklanmasorgu"],
   code: `
-  $onlyIf[$isBanned[$guildID;$message[1]]!=false;bu kullanıcı banlanmamış]
+  $onlyIf[$isBanned[$guildID;$message[1]]!=false;$title[Hata] $description[bu kullanıcı banlanmamış]]
   $onlyIf[$isNumber[$message[1]]!=false;yarrak]
   `,
 };
