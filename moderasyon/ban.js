2df5cbea6563136998a3f606317ce23d626500bf
Glitch (xfrknbot) <none>
2022-09-26 18:53:53 +0000

    ⛱🎻 Checkpoint ./moderasyon/ban.js:31098313/97

--- moderasyon/ban.js
+++ moderasyon/ban.js
@@ -2,7 +2,8 @@ module.exports = {
   name: "ban",
   aliases: ["yasakla"],
   code: `
-  $onlyPerms[ban;x;Aa...Dostum banlama yetkim yok sanırım ve ya bu banlayacağım kişiye yetkim yetmiyor. 🤔]
-  $ban[$guildID;$mentioned[1;no];;$noMentionMessage]
+  $title[Başarılı $customEmoji[onayla]]
+  $onlyPerms[ban;kick;Aa...Dostum banlama ve kickleme yetkim yok sanırım ve ya bu banlayacağım kişiye yetkim yetmiyor. 🤔]
+  $ban[$guildID;$mentioned[1];1;$noMentionMessage]
   `
 };
