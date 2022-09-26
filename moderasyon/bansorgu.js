405918c6ae98f7ed9a1dcd47e9a07b4630624875
Glitch (xfrknbot) <none>
2022-09-26 18:17:47 +0000

    👩‍❤️‍💋‍👩😽 Checkpoint ./moderasyon/ban.js:31098313/6 ./moderasyon/bansorgu.js:31098313/53

--- moderasyon/bansorgu.js
+++ moderasyon/bansorgu.js
@@ -2,7 +2,8 @@ module.exports = {
   name: "bansorgu",
   aliases: ["yasaksorgu", "yasaklanmasorgu"],
   code: `
-  $onlyIf[$isBanned[$guildID;$message[1]]!=false;$title[Hata] $description[bu kullanıcı banlanmamış]]
+  oldu ab
+  $onlyIf[$isBanned[$guildID;$message[1]]!=false;$title[Hata]]
   $onlyIf[$isNumber[$message[1]]!=false;yarrak]
   `,
 };
