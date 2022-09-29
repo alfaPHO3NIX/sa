e345367dc21b89d1087553b88bf23daf52a78b3c
Glitch (xfrknbot) <none>
2022-09-29 04:58:27 +0000

    🎟💇 Checkpoint ./server.js:31098313/44 ./eğlence/random meme.js:31098313/146

---
+++ eğlence/random meme.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "",
+  aliases: [""],
+  code: `
+$randomText[sa;ahahahahah;uSERDAR AĞAĞĞAĞAĞ;YABANCI DEĞİLİM]
+`,
+};
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ loader.load(bot.cmd, "./kasa/")
 loader.load(bot.cmd, "./premium/")
 loader.load(bot.cmd, "./sıralama/")
 loader.load(bot.cmd, "./özelkomutlar")
+loader.load(bot.cmd, "./eğlence")
 
 /////////callback/////////
 bot.onMessage()
