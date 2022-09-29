d64fb7da493c7bb14ba111e423cc492944322755
Glitch (xfrknbot) <none>
2022-09-29 05:10:26 +0000

    💗❄️ Checkpoint ./eğlence/random meme.js:31098313/336

--- eğlence/random meme.js
+++ eğlence/random meme.js
@@ -1,7 +1,11 @@
 module.exports = {
-  name: "",
-  aliases: [""],
+  name: "rmeme",
+  aliases: ["randommeme","random-meme"],
   code: `
-$randomText[sa;ahahahahah;uSERDAR AĞAĞĞAĞAĞ;YABANCI DEĞİLİM]
+$randomText[
+https://tenor.com/view/rasi%CC%87mozank%C3%BCtahyali-beyazfutbol-cahi%CC%87lertem-gif-25009199
+;https://tenor.com/view/memati-yeter-ulan-yeter-memati-yeter-ulan-yeter-yeter-ulan-yeter%C3%A7%C4%B1k%C4%B1n-lan-ortaya-kans%C4%B1zlar-kurtlar-vadisi-gif-21983572
+
+]
 `,
 };
