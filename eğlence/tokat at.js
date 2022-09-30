45820b9db56b82f068e1423b83c6211caab6e059
Glitch (xfrknbot) <none>
2022-09-30 19:03:18 +0000

    👝🔮 Checkpoint ./eğlence/tokat at.js:31098313/2

--- eğlence/tokat at.js
+++ eğlence/tokat at.js
@@ -1,7 +1,7 @@
 module.exports = {
 name: "tokatat",
 code: `
-$title[<@$mentioned[1]> Adlı kişiye ✋ tokat attın!]
+$title[<@$mentioned[1;]> Adlı kişiye ✋ tokat attın!]
 $color[$random[1;999999]]
 $description[
 
@@ -11,7 +11,7 @@ tokat atan kişi : <@$authorID>
 $image[https://media.giphy.com/media/V3pRzhx8gElm9B2Lol/giphy.gif]
 
 $footer[Made By xFurkan]
-$onlyIf[$mentioned[1]!=undefined;Kime tokat atıcaksın? havaya tokat atamazsın 😄]
+$onlyIf[$mentioned[1;]!=undefined;Kime tokat atıcaksın? havaya tokat atamazsın 😄]
 
 `
 };
