6bb5d54e462af085317790e10b64f1941c36d58f
Glitch (xfrknbot) <none>
2022-10-02 18:02:03 +0000

    🌎⛸ Checkpoint ./komutlar/önerikutusu.js:31098313/485

--- komutlar/önerikutusu.js
+++ komutlar/önerikutusu.js
@@ -2,17 +2,7 @@ module.exports = {
   name: "ökutusu",
   aliases: ["önerikutusu"],
   code: `
-  $title[Başarılı!]
-  $description[
-  Öneri kutusu başarıyla açıldı!
-  ]
-  $setVar[önerikutusu;açık]
-  $onlyIf[$message[1]==aç;
-  $setVar[önerikutusu;]
-  $onlyIf[$getVar[önerikutusu]!=kapalı;Öneri kutusu zaten kapalı!]
-  $onlyIf[$message[1]==kapat;
-  Kullanım: !önerikutusu aç/kapat]
-  ]
+ $if[$message[1]==aç]
   
-  `
+ `
 };
