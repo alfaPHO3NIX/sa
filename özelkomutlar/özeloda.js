21aab9199943cafb7db806da40ca883dd4506324
Glitch (xfrknbot) <none>
2022-10-03 16:38:47 +0000

    🐘🍈 Checkpoint ./özelkomutlar/özeloda.js:31098313/382

---
+++ özelkomutlar/özeloda.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "özeloda oluştur",
+  aliases: ["özel-oda oluştut"],
+  code: `
+  sa
+  $addButton[]
+ `,
+};
+
+module.interactionCommand({
+ name: "deneme",
+ prototype: "button",
+ code: `
+ 
+ `
+  })
