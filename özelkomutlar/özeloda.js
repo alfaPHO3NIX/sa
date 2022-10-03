688162d03352489e460a01e405e19db8720a2b76
Glitch (xfrknbot) <none>
2022-10-03 18:01:11 +0000

    ☕️🍖 Checkpoint ./özelkomutlar/özeloda.js:31098313/1671

--- özelkomutlar/özeloda.js
+++ özelkomutlar/özeloda.js
@@ -1,16 +1,29 @@
-module.exports = {
+module.exports = [{
+
   name: "özeloda oluştur",
+
   aliases: ["özel-oda oluştut","ö"],
+
   code: `
   sa
-  $addButton[1;saasab;alert;deneme;no]
- `,
-};
+  $addButton[1;saasab;green;deneme;no]
 
-module.interactionCommand({
- name: "deneme",
- prototype: "button",
- code: `
- sasa
  `
-  })
+
+},{
+
+name: "deneme",
+type: "interaction",
+prototype: "button",
+code:`
+  $onlyif[$get[authorID]==$interactionData[author.id];{
+"content" : "Bu Butonu Sadece Komutu Kullanan Kişi Basabilir",
+"ephemeral" : true,
+"options" : { "interaction" : true }
+}]
+$onlyif[$get[customId]==triggerl;]
+$let[authorID;$splitText[2]]
+$let[customId;$splitText[1]] 
+$textSplit[$interactionData[customId];_;1]
+`
+}]
