ff34a57e39e95d8709322013a3a202154d7c4d4e
Glitch (xfrknbot) <none>
2022-10-03 20:01:14 +0000

    🌒🎗 Checkpoint ./özelkomutlar/özeloda.js:31098313/765

--- özelkomutlar/özeloda.js
+++ özelkomutlar/özeloda.js
@@ -1,17 +1,45 @@
-module.exports = {
+module.exports = [{
+
   name: "özeloda oluştur",
+
   aliases: ["özel-oda oluştut","ö"],
+
   code: `
-  sasasaa
+
+  sa
+
+  $addButton[1;saasab;1;deneme_$authorID;no]
+ $onlyIf[$getGlobalUserVar[premium]==Var;{newEmbed:{color:RANDOM}{title:Özel Oda Menü * Hata}{description:Bu komutu sadece premium üyeler kullanabilir!}}]
  `
-}
-tent" : "Bu Butonu Sadece Komutu Kullanan Kişi Basabilir",
+
+},{
+
+type:"interaction",
+
+prototype: "button",
+
+code:`
+
+$interactionUpdate[sa]
+
+$onlyif[$get[authorID]==$interactionData[author.id];{
+
+"content" : "Bu Butonu Sadece Komutu Kullanan Kişi Basabilir",
+
 "ephemeral" : true,
+
 "options" : { "interaction" : true }
+
 }]
-$onlyif[$get[customId]==deneme;]
+
+$onlyIf[$get[customId]==deneme;]
+
 $let[authorID;$splitText[2]]
+
 $let[customId;$splitText[1]] 
+
 $textSplit[$interactionData[customId];_;1]
+
 `
-}]
+
+}] 
