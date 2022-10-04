718d7f6d2c501aa41c7cd437d5cede2ec7011409
Glitch (xfrknbot) <none>
2022-10-04 18:45:56 +0000

    🍔👱 Checkpoint ./özelkomutlar/sss.js:31098313/922

--- özelkomutlar/sss.js
+++ özelkomutlar/sss.js
@@ -1,9 +1,43 @@
-module.exports = {
-  name: "s",
-  aliases: ["süüü"],
+module.exports = [{
+  name: "deneme",
+  aliases: ["sa"],
   code: `
-  Özel Oda ID;
-  $getGlobalUserVar[ozelodaid]
-  
-  `
+Dene krşm
+$addButton[1;Sea;success;sea_$authorID;no]
+`
+},{
+type:"interaction",
+prototype:"button",
+code:`
+$interactionModal[CM eleştirici;bbModal;
+    {actionRow:
+      {textInput:Kaç CM AB Adı:1:ddModal:yes:Cm yaz ab:3:30}
+    }]
+$onlyif[$get[authorID]==$interactionData[author.id];{
+
+"content" : "Bu Butonu Sadece Komutu Kullanan Kişi Basabilir",
+
+"ephemeral" : true,
+
+"options" : { "interaction" : true }
+
+}]
+
+$onlyIf[$get[customId]==sea;]
+
+$let[authorID;$splitText[2]]
+
+$let[customId;$splitText[1]] 
+
+$textSplit[$interactionData[customId];_;1]
+`
+},{
+name:"bbModal",
+type:"interaction",
+prototype:"modal",
+code: `
+$interactionReply[Hmm... $textInputValue[oModal] cm iyi iyi dmmdmdmdmdmödmd]
+`
+
 }
+]
