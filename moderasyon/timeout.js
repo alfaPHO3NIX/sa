6846685cb3ae12ee810e9cf055bdaa7684ecb7b7
Glitch (xfrknbot) <none>
2022-10-05 19:05:21 +0000

    🛍👍 Checkpoint ./moderasyon/timeout.js:31098313/498

--- moderasyon/timeout.js
+++ moderasyon/timeout.js
@@ -1,10 +1,25 @@
 module.exports = [{
   name: "timeout",
-  aliases: ["zamanaşımı"],
+  aliases: ["zamanaşımı","t"],
   code: `
   $title[Zaman Aşımı]
   $description[Seçiniz.]
-  $addButton[1;asimuygula;primary]
+  $addButton[1;Aşım Uygula;primary;asimuygula_$authorID;no;⏱️]
   
+  `
+},{
+  prototype:"button",
+  type:"interaction",
+  code:`
+  deneme
+  $onlyif[$get[authorID]==$interactionData[author.id];{
+"content" : "Bu Butonu Sadece Komutu Kullanan Kişi Basabilir",
+"ephemeral" : true,
+"options" : { "interaction" : true }
+}]
+$onlyIf[$get[customId]==asimuygula;]
+$let[authorID;$splitText[2]]
+$let[customId;$splitText[1]] 
+$textSplit[$interactionData[customId];_;1]
   `
 }]
