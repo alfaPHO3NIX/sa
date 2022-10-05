f029e7410ae16975e89efac90fbded503fe16f14
Glitch (xfrknbot) <none>
2022-10-05 18:42:53 +0000

    🌒👙 Checkpoint ./sıralama/parasıralama.js:31098313/6

--- sıralama/parasıralama.js
+++ sıralama/parasıralama.js
@@ -2,7 +2,7 @@ module.exports = {
 name: "sıralama para",
 aliases: ['siralama para','leaderboard money'],
 code: `
-$title[$customEmoji[coin] Para Sıralaması]
+$title[$customEmoji[coin] Para Sıralaması;{top}]
 $description[$globalUserLeaderBoard[para]]
 `
 }
