63d7902653d0e2a8d87ef3e246809d83e3e12a02
Glitch (xfrknbot) <none>
2022-10-05 18:54:03 +0000

    👕😅 Checkpoint ./sıralama/parasıralama.js:31098313/160 ./moderasyon/timeout.js:31098313/179

---
+++ moderasyon/timeout.js
@@ -0,0 +1,10 @@
+module.exports = [{
+  name: "timeout",
+  aliases: ["zamanaşımı"],
+  code: `
+  $title[Zaman Aşımı]
+  $description[Seçiniz.]
+  $addButton[1;asimuygula;primary]
+  
+  `
+}]
--- sıralama/parasıralama.js
+++ sıralama/parasıralama.js
@@ -2,7 +2,7 @@ module.exports = {
 name: "sıralama para",
 aliases: ['siralama para','leaderboard money'],
 code: `
-$title[$customEmoji[coin] Para Sıralaması;{top}]
-$description[$globalUserLeaderBoard[para]]
+$title[$customEmoji[coin] Para Sıralaması]
+$description[$globalUserLeaderBoard[para;asc;{top} | {username}. - $customEmoji[coin]{value}]]
 `
 }
