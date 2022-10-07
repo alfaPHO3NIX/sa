935133c7835a3f0dee75ed2bf1cf3e81b49ebb8a
Glitch (xfrknbot) <none>
2022-10-07 19:35:00 +0000

    👑👰 Checkpoint ./özelkomutlar/özeloda.js:31098313/34

--- özelkomutlar/özeloda.js
+++ özelkomutlar/özeloda.js
@@ -1,51 +1,31 @@
 module.exports = [{
-
   name: "özeloda",
-
   aliases: ["özel-oda","ö"],
-
   code: `
-
-  
   $title[Özel Oda Menü]
   $addButton[1;Oda Oluştur;success;odaolustur_$authorID;no]
  $addButton[1;Oda Bilgileri;1;odabilgileri_$authorID;no]
  $onlyIf[$getGlobalUserVar[premium]==Var;{newEmbed:{color:RANDOM}{title:Özel Oda Menü * Hata}{description:Bu komutu sadece premium üyeler kullanabilir!}}]
  `
-
 },{
 
-type:"interaction",
-
+  type:"interaction",
 prototype: "button",
-
 code:`
-      
         $interactionModal[Özel Oda Oluştur;sModal;
     {actionRow:
       {textInput:Özel Oda Adı:1:oModal:yes:Oda Adı:3:30}
     }]
-
 $onlyif[$get[authorID]==$interactionData[author.id];{
-
 "content" : "Bu Butonu Sadece Komutu Kullanan Kişi Basabilir",
-
 "ephemeral" : true,
-
 "options" : { "interaction" : true }
-
 }]
-
 $onlyIf[$get[customId]==odaolustur;]
-
 $let[authorID;$splitText[2]]
-
 $let[customId;$splitText[1]] 
-
 $textSplit[$interactionData[customId];_;1]
-
 `,
-
 },{
   name: "sModal",
   type: "interaction",
