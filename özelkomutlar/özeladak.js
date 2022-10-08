493b058aa7f4fee96c378dbe64cdfeaf37cb565a
Glitch (xfrknbot) <none>
2022-10-08 12:19:47 +0000

    🏗🍿 Checkpoint ./özelkomutlar/özeloda.js:31098313/97 ./özelkomutlar/özeladak.js:31098313/217

---
+++ özelkomutlar/özeladak.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "özelodaadmin",
+  aliases: ["özeloda-admin","ö admin"],
+  code: `
+  $title[Özel Oda | Admin Menüsü]
+  $description[Seçim Yapınız.]
+  $addButton[]
+  
+  `
+}
