f4789a218e5643934851267d33f7581b4ac51f2c
Glitch (xfrknbot) <none>
2022-10-08 12:31:47 +0000

    🔨🎯 Checkpoint ./özelkomutlar/özeladak.js:31098313/636

--- özelkomutlar/özeladak.js
+++ özelkomutlar/özeladak.js
@@ -1,10 +1,29 @@
-module.exports = {
+module.exports = [{
   name: "özelodaadmin",
   aliases: ["özeloda-admin","ö admin"],
   code: `
   $title[Özel Oda | Admin Menüsü]
   $description[Seçim Yapınız.]
-  $addButton[]
+  $addButton[1;Kategori ID Ayarla;1;katidayarla_$authorID]
   
   `
-}
+},{
+  type:"interaction",
+  prototype:"button",
+  code:`
+  xlsls
+  $onlyIf[$channelExists[$textInputValue[kModal]]
+  $onlyif[$get[authorID]==$interactionData[author.id];{
+"content" : "Bu Butonu Sadece Komutu Kullanan Kişi Basabilir",
+"ephemeral" : true,
+"options" : { "interaction" : true }
+}]
+$onlyIf[$get[customId]==katidayarla;]
+$let[authorID;$splitText[2]]
+$let[customId;$splitText[1]] 
+$textSplit[$interactionData[customId];_;1] 
+  `
+  
+  
+  
+}]
