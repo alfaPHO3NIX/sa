cb922bfc373c7465f8f0b5746f3e780c4bcb47fb
Glitch (xfrknbot) <none>
2022-10-12 20:09:52 +0000

    🎋🏕 Checkpoint ./moderasyon/stats.js:31098313/110

--- moderasyon/stats.js
+++ moderasyon/stats.js
@@ -4,11 +4,13 @@ module.exports = [{
   name:"stats",
   code: `
   $interactionReply[;{newEmbed:
+  {color:RANDOM}
   {title:İstatistikler}
   {description:
-  **Bot İstatistikleri**
   Kullanılan Ram: $ram
+  
   Uptime Süresi: $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$uptime;hour; Saat];hours;Saat];minutes; Dakika];minute; Dakika];seconds; Saniye];second; Saniye]
+  
   Bot Sahibi: <@$botOwnerID>
   }}
   `
