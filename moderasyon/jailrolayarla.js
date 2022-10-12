a3d686872d2131ebaa33b8b65c83aa82f31a6889
Glitch (xfrknbot) <none>
2022-10-12 20:44:19 +0000

    🏍💓 Checkpoint ./moderasyon/jailrolayarla.js:31098313/760

--- moderasyon/jailrolayarla.js
+++ moderasyon/jailrolayarla.js
@@ -1,9 +1,26 @@
 module.exports = {
+  name: "jailrolayarla",
+  aliases: ["hapisrolayarla"],
+  code: `
+  $color[random]
+  $title[Başarılı!]
+  $description[
+  Rol <&$mentionedRoles[1]> olarak ayarlandı.
+  ]
+  $setServerVar[jailroleid;$mentionedRoles[1]]
+  $onlyIf[$mentionedRoles[1]!=undefined;
+  {newEmbed:
+  {title: Hata}
+  {description: Lütfen bir rol belirt!}
+  }
 
-  name: "",
-
-  aliases: [""],
-
-  code: ``
-
+]
+ 
+ 
+  $onlyPerms[admin;{newEmbed:
+  {title: Hata}
+  {description:Görünüşe göre kanal id si ayarlanmamış.\n\nEğer bunu gördüyseniz lütfen bir yetkiliye bildiriniz.
+  }
+]
+  `
 }
