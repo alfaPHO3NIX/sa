21c81a07efb498039eb42456525481476fd113c6
Glitch (xfrknbot) <none>
2022-10-16 06:47:30 +0000

    💇⛑ Checkpoint ./komutlar/parasil.js:31098313/570

---
+++ komutlar/parasil.js
@@ -0,0 +1,14 @@
+module.exports = {
+name: "parasil",
+code: `
+$title[Başarılı!]
+$description[** **
+<@$mentioned[1]> adlı kişiden $numberSeparator[$noMentionMessage[1;no]] silindi.
+** **]
+$color[RANDOM]
+
+$onlyIf[$isNumber[$noMentionMessage[1]]!=false;**<@$authorID> Lütfen bir miktar belirt**]
+$onlyForIDs[$botOwnerID;Yetkin Yok.]
+$setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$mentioned[1]];$noMentionMessage[1]];$mentioned[1]]
+`
+}
