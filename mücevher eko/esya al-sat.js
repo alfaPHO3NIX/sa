b16bf2b47ef1cebcb5c245b693e3f09bfb3df467
Glitch (xfrknbot) <none>
2022-10-17 13:25:33 +0000

    🏗👧 Checkpoint ./mücevher eko/esya al-sat.js:31098313/397

--- mücevher eko/esya al-sat.js
+++ mücevher eko/esya al-sat.js
@@ -1,16 +1,9 @@
-module.exports = [{
-  name: "satınal",
-  aliases: ["buy"],
+module.exports = {
+  name: "satınal worldlock",
+  aliases: ["buy worldlock","satinal wl"],
   code: `
-  Seç
-  $addSelectMenu[1;helpCustomID;This placeholder won't show up cause we have selected default field as yes;1;1;no;A Option:Description of A option:helpValue0:no:👋;B Option::helpValue1:yes]
-
-  `
-},{
-  name:"helpCustomID",
-  prototype:"selectmenu",
-  code:`
-  sa
-  `
+  za
   
-}]
+  $onlyIf[$getGlobalUserVar[mucevher]>=10;Yetersiz Gem.]
+  `
+  }
