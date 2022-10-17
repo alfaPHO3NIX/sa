994d3c09dadb1a5941d8cee242ddc777d8470021
Glitch (xfrknbot) <none>
2022-10-17 16:42:30 +0000

    💄📻 Checkpoint ./Etkinlik/kaz.js:31098313/105

--- Etkinlik/kaz.js
+++ Etkinlik/kaz.js
@@ -3,11 +3,11 @@ module.exports = {
   aliases: ["dig"],
   code: `
   $if[$getGlobalUserVar[mevcutblok]==toprak;
-  $setGlobalUserVar[mücevher;$sum[$getglobalUserVar[mücevher];random[$getVar[toprakmingem];$getVar[toprakmaxgem]]]]
+  $setGlobalUserVar[mücevher;$sum[$getglobalUserVar[mücevher];$random[$getVar[toprakmingem];$getVar[toprakmaxgem]]]]
   $title[Blok Kazıldı]
   $description[Kazılan Blok: Toprak
-
-]
+  Kazanılan Mücevher: $random[$getVar[toprakmingem];$getVar[toprakmaxgem]]
+  ]
   ;
   olmadı be yarrak
   ]
