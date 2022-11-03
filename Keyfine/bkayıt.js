e909c09074cf523b0c28bf738ba17e66cc2fa4f8
Glitch (xfrknbot) <none>
2022-11-03 17:27:51 +0000

    😋👋 Checkpoint ./Keyfine/bkayıt.js:31098313/329

--- Keyfine/bkayıt.js
+++ Keyfine/bkayıt.js
@@ -1,7 +1,23 @@
-module.exports = {
-  name: "ö",
+module.exports = [{
+  name: "ösa",
   aliases: [""],
   code: `
-  
+  $title[Snors Code]
+  $description[Kayıt Olmak için Tıkla!]
+  $addButton[1;Kayıt Ol;success;kayitol;no]
   `
-}
+},{
+  name:"kayitol",
+  prototype:"button",
+  type:"interaction",
+  code:`
+  $interactionModal[Kayıt Ol;kayitModal;
+  {actionRow:
+     
+  
+  
+  }]
+  `
+  
+  
+}]
