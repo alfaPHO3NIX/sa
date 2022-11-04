c475cc16bad6816a005f9c2065780079b3ddc6d7
Glitch (xfrknbot) <none>
2022-11-04 08:05:36 +0000

    🛬🚘 Checkpoint ./Etkinlik/blokyuksel.js:31098313/451

--- Etkinlik/blokyuksel.js
+++ Etkinlik/blokyuksel.js
@@ -2,10 +2,9 @@ module.exports = {
   name: "blokyükselt",
   aliases: ["blokyüksel"],
   code: `
-  $if[$getGlobalUserVar[mevcutblok]==toprak;
-  $if[$getGlobalUserVar[mücevher]>=100;$setGlobalUserVar[mevcutblok;elmasblok]]
-  Başarılı
-  ; as
+  $onlyIf[$getGlobalUserVar[mücevher]>=$getGlobalUserVar[$getGlobalUserVar[mevcutblok]nblok];**Mücevherin Yetersiz!
+  Gereken Mücevher: $getGlobalUserVar[$getGlobalUser]
+  ]
   ]
  `
 }
