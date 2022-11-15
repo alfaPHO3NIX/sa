d598e305363dbba37369160eba6786d3f2b55d27
Glitch (xfrknbot) <none>
2022-11-15 23:59:36 +0000

    🚁📖 Checkpoint ./özelkomutlar/özeladak.js:31098313/62

--- özelkomutlar/özeladak.js
+++ özelkomutlar/özeladak.js
@@ -74,8 +74,8 @@ $textSplit[$interactionData[customId];_;1]
   code :`
   
   $interactionReply[Talep Logu Başarıyla Ayarlandı $customEmoji[onayla];;;;;yes]
-  $setVar[öismdegistlplog;$textInputValue[öismdegistlplog]]
-  $onlyIf[$channelExists[$textInputValue[öismtlplog]]==true;$interactionReply[;{newEmbed:{title:Özel Oda | Admin Menüsü | İsim Değişim Talep Logu Ayarla * Hata}{description:\n** **$customEmoji[reddet] Böyle bir kanal bulunamadı!\n** **}};;;;yes]]
+  $setVar[öismdegistlplog;$textInputValue[öismtlplog]]
+  $onlyIf[$serverChannelExists[$textInputValue[öismtlplog]]!=false;$interactionReply[;{newEmbed:{title:Özel Oda | Admin Menüsü | İsim Değişim Talep Logu Ayarla * Hata}{description:\n** **$customEmoji[reddet] Böyle bir kanal bulunamadı!\n** **}};;;;yes]]
   `
   
   
