8865b0f0b55edad266b395fa3d2af54174a92958
Glitch (xfrknbot) <none>
2022-11-17 13:09:25 +0000

    🍤✋ Checkpoint ./özelkomutlar/emojilikonuş.js:31098313/596

--- özelkomutlar/emojilikonuş.js
+++ özelkomutlar/emojilikonuş.js
@@ -5,10 +5,25 @@ module.exports = {
 $deletecommand
 $title[1;$username]
 $description[
-$replaceText[$replaceText[$message;
-;:;\:]]
-;:;:
+$replacetext[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$message
 
+;:bughunter:;$customEmoji[bughunter]]
+
+;:verified:;$customEmoji[verified]]
+
+;:kirmiziyildiz:;$customEmoji[kirmiziyildiz]]
+
+;:tac:;$customEmoji[emoji_003]]
+
+;:onayla:;$customEmoji[onayla]]
+
+;:reddet:;$customEmoji[reddet]]
+
+;(;$customEmoji[]]
+
+;);:]
+
+;:partner:;$customEmoji[partner]]
 $argsCheck[>0;En azından 1 kelime yazsaydın ya be kanka!]
 $onlyIf[$getGlobalUserVar[premium]==Var;Bu Özellik Sadece Premium Üyelere Özeldir$customEmoji[reddet]]
 
