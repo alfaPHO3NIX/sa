463219b5981988a5e0bb25e4a469ae90906123c1
Glitch (xfrknbot) <none>
2022-11-18 00:02:01 +0000

    🏅💍 Checkpoint ./özelkomutlar/özeloda.js:31098313/336

--- özelkomutlar/özeloda.js
+++ özelkomutlar/özeloda.js
@@ -14,6 +14,8 @@ module.exports = [{
   prototype: "button",
   code: `
   $interactionReply[Oda Başarıyla Oluşturuldu! <#$channelID[$random[1000;9999]-özel-oda]>;;;;;yes]
+ $setGlobalUserVar[ozelodaid;$channelID[$random[1000;9999]-özel-oda]]
+ $modifyChannelPerms[$authorID;$channelID[$random[1000;9999]-özel-oda];+viewchannel;+sendmessage;+addreactions]
  $createChannel[$guildID;$random[1000;9999]-özel-oda;text;no;$getVar[ozelodakategori]]
  $setGlobalUserVar[ozelodaisim;$random[1000;9999]-özel-oda]
  $setGlobalUserVar[ozeloda;var]
