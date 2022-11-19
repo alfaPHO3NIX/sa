32d9d31820b7f8079c75614d0ec93cb5c60d17ef
Glitch (xfrknbot) <none>
2022-11-19 22:41:31 +0000

    🏥🙈 Checkpoint ./komutlar/rozetler.js:31098313/44

--- komutlar/rozetler.js
+++ komutlar/rozetler.js
@@ -11,7 +11,7 @@ $customEmoji[premium] Premium Sahibi: $getGlobalUserVar[premiumsahibirozet]
 \n$customEmoji[etkinlikkazanani] Etkinlik Kazananı: $getGlobalUserVar[etkinlikkazananırozet]
 \n$customEmoji[verified] Onaylı Kişi: $getGlobalUserVar[onaylıkişirozet]
 \n$customEmoji[kirmiziyildiz] Developer: $getGlobalUserVar[geliştiricirozet]
-\n💰 Destekçi: $getGlobalUserVar[destekçirozet]
+\n$customEmoji[destekci] Destekçi: $getGlobalUserVar[destekçirozet]
 $color[RANDOM]
 ]
 `
