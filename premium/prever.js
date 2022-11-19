30e6dd8facdd4b01ca248faa5974b953253db6da
Glitch (xfrknbot) <none>
2022-11-19 18:54:59 +0000

    🚿🏘 Checkpoint ./server.js:31098313/10 ./premium/prever.js:31098313/92

--- premium/prever.js
+++ premium/prever.js
@@ -3,13 +3,13 @@ name: "prever",
 aliases: ['premiumver'],
 code: `
 $title[$customEmoji[verified]Başarılı]
-$description[<@$mentioned[1;no]> Adlı Kişiye Premium Verildi!
+$description[<@$mentioned[1;no]> Adlı Kişiye $noMentionMessage[1] PreCredit Verildi!
 \n$customEmoji[premium]Premium Rozeti Kazanıldı
 \n<@&$getVar[prerol]> Rolü Verildi $customEmoji[onayla]]
 $giveRoles[688736441290719256;$mentioned[1];$getVar[prerol]]
 $setGlobalUserVar[premiumsahibirozet;Sahipsin$customEmoji[onayla];$mentioned[1]]
 $setGlobalUserVar[premium;Var;$mentioned[1]]
-$setGlobalUserVar[precredit;$sum[$getGlobalUserVar[premium;$mentioned[1;no]];$noMentionMessage[1]]]
+$setGlobalUserVar[precredit;$sum[$getGlobalUserVar[precredit;$mentioned[1;no]];$noMentionMessage[1]];$mentioned[1;no]]
 $onlyIf[$isNumber[$noMentionMessage[1]]!=false;Bir Sayı Belirtmelisin $customEmoji[reddet]]
 $onlyIf[$mentioned[1;no]!=undefined;Bir Kişi Belirtmelisin $customEmoji[reddet]]
 $onlyForIDs[980089372768624660;711923709253910588;x;Yetkin Yok $customEmoji[reddet]]
